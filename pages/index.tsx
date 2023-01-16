a0c5e72851f2ecf5c4ae7a0b8a449d49655f460a
joonmo-you <joonmo.you@gmail.com>
2023-01-16 16:28:55 +0900

    demo: render with generating random hex color

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,26 @@
-import type { NextPage } from "next";
+import styled from "@emotion/styled";
+
+import TestRunner from "../src/bench/TestRunner";
+
+const View = styled.div<{ backgroundColor: string }>`
+  widht: 150px;
+  height: 150px;
+  backgroundcolor: ${(props) => props.backgroundColor};
+`;
+
+const StyledHeading = styled.h1<{ color: string }>`
+  font-size: 100px;
+  color: ${(props) => props.color};
+`;
 
 function Home() {
-  return <div></div>;
+  return (
+    <TestRunner numberOfTest={3} iterationNumber={10000}>
+      <View backgroundColor={"#".concat(Math.floor(Math.random() * Math.pow(16, 6)).toString(16))}>
+        <StyledHeading color={"#".concat(Math.floor(Math.random() * Math.pow(16, 6)).toString(16))}>BOX</StyledHeading>
+      </View>
+    </TestRunner>
+  );
 }
 
 export default Home;
