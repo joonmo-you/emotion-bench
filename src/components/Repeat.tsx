ce8575b88ded64e3ec476b005a325dfaae3b1c50
joonmo-you <joonmo.you@gmail.com>
2023-01-16 16:27:59 +0900

    refacotr: enable children of repeat to map with key prop

--- src/components/Repeat.tsx
+++ src/components/Repeat.tsx
@@ -1,12 +1,18 @@
 import * as React from "react";
-import { pipe, repeat, toArray } from "@fxts/core";
+import { pipe, range, toArray } from "@fxts/core";
 
 interface Props extends React.PropsWithChildren {
   count: number;
 }
 
 function Repeat({ count, children }: Props) {
-  return count > 0 ? <>{pipe(repeat(count, children), toArray)}</> : null;
+  return count > 0 ? (
+    <>
+      {pipe(range(count), toArray).map((_, index) => (
+        <React.Fragment key={index}>{children}</React.Fragment>
+      ))}
+    </>
+  ) : null;
 }
 
 export default Repeat;
